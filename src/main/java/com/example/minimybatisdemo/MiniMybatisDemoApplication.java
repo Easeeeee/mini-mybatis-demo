26066f99b3dfb1c5db7a534b884ecc46e3d443af
zyz <724469255@qq.com>
2023-02-14 15:41:35 +0800

    项目初始化

---
+++ src/main/java/com/example/minimybatisdemo/MiniMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.minimybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMybatisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minimybatisdemo/MiniMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minimybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMybatisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
